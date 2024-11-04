42caed55e15a92a18b1823d71e1ea683942d6491
CaptainK0stas <captainkostas76@gmail.com>
2024-11-04 19:07:34 +0200

    Add files via upload

---
+++ project/user_management/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128) 
+    
+class Person(User):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length= 50)
+    address = models.CharField(max_length= 50)
+    phone_number = models.IntegerField(max_length = 10)
+    
+    
+
+    
