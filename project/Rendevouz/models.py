f73cae6c1981d6a189b7f5426ce1bf458abff6ff
Patriarxhs <118316397+Patriarxhs@users.noreply.github.com>
2024-11-04 18:59:10 +0200

    appointment_table

---
+++ project/Rendevouz/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from project.my_app.models import User
+
+class Dog(models.Model):
+    breed=models.CharField(max_length=30)
+    age=models.SmallIntegerField()
+    dog_name=models.TextField()
+    owner=models.ForeignKey(User , on_delete=models.CASCADE)
+
+
+
+
+
+class Appointment_booking(models.Model):
+    time=models.DateTimeField()
+    avaiable_tasks = [
+    ("WK" , "Walk"),
+    ("PT" , "Pet taxi"),
+    ("PS" , "Pet sitting"),
+]
+    task=models.CharField(max_length=2,choices=avaiable_tasks)
+    owner =models.ForeignKey(User , on_delete=models.CASCADE)
+    
+
